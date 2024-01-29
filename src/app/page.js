8bde2cd3f952eb9d0d16d2095335ae3a3db60e0e
Artur <artur3087@gmail.com>
2024-01-29 16:50:30 +0000

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,30 @@
 export default function Home () { // START of default function Home () //
 
 
+  // Below I show a way of making array which will HARDCODE list of whatever I need - it will be a list of recaps //
+
+  const recaps = [   // START of array recaps //
+    { name: "recap1", importance: "importance: high", time: "time: long", vital: true},
+    { name: "recap2", importance: "importance: low", time: "time: too long", vital: false},
+    {name: "recap3", importance: "importance: vital", time: "time: far too long, bro!", vital: true},
+  ]; // END of array recaps //
+
+
+
 return ( // START for returning inside Home () //
 
 
   <div>
-    <h1>HOME PAGE:</h1>
-    <p>Main page</p>
+    <h2>HOME PAGE:</h2>
+    <h3>Main:</h3>
+    {recaps.map((recap) => {
+      return <div className="array" key={recap.name}>
+        <h4>{recap.name}, {recap.importance}, {recap.time}</h4>
+        <p>{recap.vital ? "Must learn it!" : "You can skip that"}</p>  {/* Here I used tenary operator which will show us which recap is must to learn in condition saying that value of vital is true */}
+        
+      </div>
+    })}
+    
   </div>
 
 
